8370b89c32facf365469efe046d9a8cea2c1c133
ram1017 <89736374+ram1017@users.noreply.github.com>
2025-07-10 15:47:04 +0530

    Update page.tsx

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -2,13 +2,15 @@ import { getClient } from "@/lib/ssrApolloClient";
 import { GET_BOOK_BY_ID } from "@/graphql/queries";
 import BookDetailsClient from "@/components/BookDetialsClient";
 
-
 export const dynamic = "force-dynamic";
+
 
 export default async function BookPage({
   params,
 }: {
-  params: { id: string };
+  params: {
+    id: string;
+  };
 }) {
   const { data } = await getClient().query({
     query: GET_BOOK_BY_ID,
